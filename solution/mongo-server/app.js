55635d188c932c5882a55494007c4b19705030b7
Juan González Pardo <870313@unizar.es>
2025-12-22 22:12:07 +0100

    Controller and models created for stats

---
+++ solution/mongo-server/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const logger = require("morgan");
+
+const app = express();
+//Middlewares execute before the requests
+app.use(logger("dev"));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/health", (req, res) => {
+  res.json({ status: "OK", server: "mongo-server" });
+});
+
+module.exports = app;
